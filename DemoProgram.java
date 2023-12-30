2b0154f7abb50c5fa7ea478adb3eb92006d2a02a
Ankit <ankitsatpute15@gmail.com>
2023-12-30 23:13:20 +0530

    added Ankit

---
+++ DemoProgram.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class DemoProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		ArrayList<String> list=new ArrayList<String>();    
+	      list.add("Sachin Tendulkar");    
+	      list.add("Mahendra Dhoni");    
+	      list.add("Rohit Sharma");    
+	      list.add("Virat Kohli"); 
+	      
+	      Collections.sort(list,(e1,e2)->{  
+	    	  
+	    	  String [] e1Array =e1.split(" "); 
+	    	  String [] e2Array =e2.split(" "); 
+
+	    		  
+	    	  
+	    	  
+	    	  return e2Array[1].compareTo(e1Array[1]);
+	      });
+	    	  
+	      System.out.println(list);
+		
+	}
+
+}
